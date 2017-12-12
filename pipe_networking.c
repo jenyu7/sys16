0210fbfe66ecc82a3163134a96e479046571c623
BrianLim167 <blim167@gmail.com>
2017-12-12 15:35:09 -0500

    made fifo in server

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,12 +5,17 @@
   server_handshake
   args: int * to_client
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
 
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  if ( mkfifo("wellknown_pipe", 0644); ) {
+    // if mkfifo gives -1, an error happened
+    printf("SERVER: fifo creation failed");
+    return 1;
+  }
   return 0;
 }
 
@@ -19,7 +24,7 @@ int server_handshake(int *to_client) {
   client_handshake
   args: int * to_server
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
 
   returns the file descriptor for the downstream pipe.
