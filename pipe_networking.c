accff5d2ce00c5097f257fd9c0b8dcb8db163d04
BrianLim167 <blim167@gmail.com>
2017-12-13 15:24:57 -0500

    wrote server_handshake

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,12 +11,37 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  if ( mkfifo("wellknown_pipe", 0644); ) {
+  if ( mkfifo("wellknown_pipe", 0644) == -1 ) {
     // if mkfifo gives -1, an error happened
-    printf("SERVER: fifo creation failed");
-    return 1;
+    printf("SERVER: %d %s", errno, strerror(errno));
+    exit(1);
   }
-  return 0;
+  char clients_message[256];
+  int *from_client = open("wellknown_pipe", O_RDONLY);
+  if ( *from_client == -1 ) {
+    // if open gives -1, an error happened
+    printf("SERVER: %d %s", errno, strerror(errno));
+    exit(1);
+  }
+  memset(clients_message, 0, 256); // set everything in clients_message to 0
+  read(*from_client, clients_message, 256); // client's secret pipe name
+  close(*from_client);
+  *to_client = open(clients_message, O_WRONLY);
+  if ( *to_client == -1 ) {
+    // if open gives -1, an error happened
+    printf("SERVER: %d %s", errno, strerror(errno));
+    exit(1);
+  }
+  write(*to_client, "got it, respond back pls", 256);
+  memset(clients_message, 0, 256); // set everything in clients_message to 0
+  read(*from_client, clients_message, 256);
+  if ( strcmp(clients_message,"got it, here's my response") != 0 ) {
+    // unexpected message
+    printf("Server: unexpected message from client for third handshake");
+    exit(1);
+  }
+  
+  return *from_client;
 }
 
 
